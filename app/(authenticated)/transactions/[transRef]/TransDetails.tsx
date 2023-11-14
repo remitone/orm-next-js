b643fa92a8ba427d5404429be4a7fc20c81c3fc2
Sharfaz Mohamed Sairaz <sharfaz@remitone.com>
2023-11-14 12:44:59 +0000

    Refactored trans details page

---
+++ app/(authenticated)/transactions/[transRef]/TransDetails.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import TransactionCard from "@/app/(authenticated)/transactions/TransactionCard";
+import { getSession } from "@/services/authentication/CookieSession";
+import { getTenantConfigs } from "@/utils/settings";
+import ApiClient from "@/services/api/remitterws/client";
+import { TransactionDetailsResponse } from "@/types/transaction-details";
+
+async function getTransaction(transRef: string) {
+  const authSession = await getSession();
+  const tenantConfigs = await getTenantConfigs();
+
+  const apiResponse = await ApiClient.post({
+    url: `${tenantConfigs.baseUrl}/transaction/getTransaction`,
+    body: {
+      username: authSession?.username,
+      session_token: authSession?.sessionToken,
+      trans_ref: transRef,
+    },
+    options: {
+      next: { tags: ["collection"] },
+    },
+  });
+
+  return apiResponse.response as TransactionDetailsResponse;
+}
+
+export default async function TransDetails({ transRef }: { transRef: string }) {
+  const transactionDetailsResponse = await getTransaction(transRef);
+  const transaction = transactionDetailsResponse?.result?.transaction;
+
+  return <TransactionCard transaction={transaction!} />;
+}
--- app/(authenticated)/transactions/[transRef]/page.tsx
+++ app/(authenticated)/transactions/[transRef]/page.tsx
@@ -1,10 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Box, Space, Text } from "@mantine/core";
-import { getTenantConfigs } from "@/utils/settings";
-import ApiClient from "@/services/api/remitterws/client";
-import { TransactionDetailsResponse } from "@/types/transaction-details";
-import TransactionCard from "@/app/(authenticated)/transactions/TransactionCard";
-import { getSession } from "@/services/authentication/CookieSession";
+import Loading from "@/app/(authenticated)/dashboard/loading";
+import TransDetails from "@/app/(authenticated)/transactions/[transRef]/TransDetails";
 
 type TransactionProps = {
   params: {
@@ -12,57 +9,8 @@ type TransactionProps = {
   };
 };
 
-async function getTransaction(transRef: string) {
-  const authSession = await getSession();
-  const tenantConfigs = await getTenantConfigs();
-
-  const apiResponse = await ApiClient.post({
-    url: `${tenantConfigs.baseUrl}/transaction/getTransaction`,
-    body: {
-      username: authSession?.username,
-      session_token: authSession?.sessionToken,
-      trans_ref: transRef,
-    },
-    options: {
-      next: { tags: ["collection"] },
-    },
-  });
-
-  return apiResponse.response as TransactionDetailsResponse;
-}
-
 export default async function Transaction({ params }: TransactionProps) {
   const transRef = params.transRef;
-  // @ts-ignore
-  //const { baseUrl } = useContext(AppSettingsContext);
-  //const session = useAuthSession();
-  // const { transaction, isLoading, isError } = useTransaction(
-  //   baseUrl,
-  //   session.username,
-  //   session.sessionToken,
-  //   transRef,
-  // );
-  const transactionDetailsResponse = await getTransaction(transRef);
-  const transaction = transactionDetailsResponse?.result?.transaction;
-
-  //   <Suspense fallback={<Loading />}>
-  //
-  //   </Suspense>
-  // if (isError) {
-  //   return (
-  //     <Notification color="red" title="Error!" withCloseButton={false}>
-  //       Failed to load the transaction {transRef} details. Try again!
-  //     </Notification>
-  //   );
-  // }
-  //
-  // if (isLoading) {
-  //   return (
-  //     <Box pos={"absolute"} top={"50%"} left={"50%"} right={"auto"}>
-  //       <Loader color="blue" />
-  //     </Box>
-  //   );
-  // }
 
   return (
     <Box pos={"relative"}>
@@ -70,7 +18,9 @@ export default async function Transaction({ params }: TransactionProps) {
         Transfer Details
       </Text>
       <Space pb={20}></Space>
-      <TransactionCard transaction={transaction!} />
+      <Suspense fallback={<Loading />}>
+        <TransDetails transRef={transRef} />
+      </Suspense>
     </Box>
   );
 }
